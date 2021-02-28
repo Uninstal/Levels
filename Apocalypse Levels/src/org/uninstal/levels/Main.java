b0e887197cb05e16a2990235dc5e53e0db01690c
Uninstal <Uninstal@192.168.0.104>
2021-02-28 14:19:11 +0500

    Release

---
+++ Apocalypse Levels/src/org/uninstal/levels/Main.java
@@ -0,0 +1,26 @@
+package org.uninstal.levels;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return super.onCommand(sender, command, label, args);
+	}
+}
---
+++ Apocalypse Levels/src/org/uninstal/levels/data/PerkType.java
@@ -0,0 +1,9 @@
+package org.uninstal.levels.data;
+
+public enum PerkType {
+
+	Dexterity, Eloquence, MetalLeather,
+	Survivability, Walker, Craft,
+	Cooking, Medicine, Hunter,
+	SaveEnergy;
+}
